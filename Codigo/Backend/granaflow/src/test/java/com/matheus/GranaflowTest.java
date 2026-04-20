7c70f0f2e0cecee4f93290d9dde6ffa51a01277f
mthsz-dm <matheusdiasmendespop@gmail.com>
2026-04-20 19:12:40 -0300

    Backend com micronout inicial

---
+++ Codigo/Backend/granaflow/src/test/java/com/matheus/GranaflowTest.java
@@ -0,0 +1,22 @@
+package com.matheus;
+
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class GranaflowTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
